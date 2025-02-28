7d82e240661e42448bdcbf7935001a2988b76756
Kimsunmin <sunmin0406@naver.com>
2025-02-28 14:27:04 +0900

    fix: Create fcm base message

--- src/pages/api/sendNotification.ts
+++ src/pages/api/sendNotification.ts
@@ -1,5 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { messaging } from '@/utils/fcm';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { messaging } from "@/utils/fcm";
 
 interface FCMMessage {
   to: string;
@@ -10,21 +10,50 @@ interface FCMMessage {
   data?: { [key: string]: string };
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
     const { to, notification, data }: FCMMessage = req.body;
 
     try {
+      const { title, body } = notification;
       const response = await messaging.send({
         token: to,
-        notification,
+        android: {
+          notification: {
+            title,
+            body,
+          },
+          priority: "high",
+        },
+        apns: {
+          payload: {
+            aps: {
+              alert: {
+                title,
+                body,
+              },
+              priority: "high",
+              contentAvailable: true,
+            },
+          },
+        },
+        webpush: {
+          notification: {
+            title,
+            body,
+          },
+        },
         data,
       });
+      console.log("Successfully sent message:", response);
       res.status(200).json({ success: true, response });
     } catch (error) {
       res.status(500).json({ success: false, error: error });
     }
   } else {
-    res.status(405).json({ success: false, message: 'Method Not Allowed' });
+    res.status(405).json({ success: false, message: "Method Not Allowed" });
   }
 }
